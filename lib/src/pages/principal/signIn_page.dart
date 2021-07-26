ef649287f5539708c41e96a9412512c9e62dfac1
Eduardo Olivo <keolivor@utn.edu.ec>
2021-07-26 09:56:15 -0500

    Refactorización del archivo signIn_page

--- lib/src/pages/principal/signIn_page.dart
+++ lib/src/pages/principal/signIn_page.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:flutter_viaje_express_cliente/src/bloc/signIn_bloc/signin_bloc.dart';
-import 'package:flutter_viaje_express_cliente/src/widgets/custom_button.dart';
-import 'package:flutter_viaje_express_cliente/src/widgets/custom_dropDown.dart';
-import 'package:flutter_viaje_express_cliente/src/widgets/custom_input.dart';
-import 'package:flutter_viaje_express_cliente/src/widgets/labels.dart';
 import 'package:flutter_viaje_express_cliente/src/widgets/logo_signin.dart';
+import 'package:flutter_viaje_express_cliente/src/widgets/signIn_widget/forms.dart';
 
 class SignInPage extends StatelessWidget {
   @override
@@ -46,165 +43,19 @@ class SiguientePanel extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<SigninBloc, SigninState>(builder: (_, state) {
       if (state.opcion == 0) {
-        return _Form();
+        return Form0();
       } else if (state.opcion == 1) {
-        return _Form1();
+        return Form1();
       } else if (state.opcion == 2) {
-        return _Form2();
+        return Form2();
       } else {
-        return _Form();
+        return Form0();
       }
     });
   }
 }
 
-class _Form extends StatefulWidget {
-  @override
-  __FormState createState() => __FormState();
-}
 
-class __FormState extends State<_Form> {
-  final cedulaCtrl = TextEditingController();
-  final nombresCtrl = TextEditingController();
-  final apellidosCtrl = TextEditingController();
- 
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return Container(
-      margin: EdgeInsets.only(top: 20),
-      padding: EdgeInsets.symmetric(horizontal: 50),
-      child: Column(
-        children: <Widget>[
-          CustomInput(
-              icon: Icons.perm_identity,
-              placeHolder: 'Cédula',
-              keyboardType: TextInputType.number,
-              textController: cedulaCtrl),
-          CustomInput(
-              icon: Icons.perm_identity,
-              placeHolder: 'Nombres',
-              keyboardType: TextInputType.name,
-              textController: nombresCtrl),
-          CustomInput(
-              icon: Icons.perm_identity,
-              placeHolder: 'Apellidos',
-              keyboardType: TextInputType.name,
-              textController: apellidosCtrl),
-
-          
-
-          CustomButton(
-              text: 'Siguiente',
-              onPressed: () {
-                BlocProvider.of<SigninBloc>(context).add(CambiarPanel(1));
-              }),
-          SizedBox(height: 35),
-          Labels(
-            ruta: 'login',
-            titulo: '¿Ya tienes cuenta?',
-            subtitulo: '¡Ingresa ahora!',
-          ),
-        ],
-      ),
-    );
-  }
-
-
-}
-
-class _Form1 extends StatefulWidget {
-  @override
-  __Form1State createState() => __Form1State();
-}
-
-class __Form1State extends State<_Form1> {
-  final telefonoCtrl = TextEditingController();
-   List<String> listaGenero = ['masculino', 'femenino', 'otro'];
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 20),
-      padding: EdgeInsets.symmetric(horizontal: 50),
-      child: Column(
-        children: <Widget>[
-          CustomInput(
-              icon: Icons.perm_identity,
-              placeHolder: 'Teléfono',
-              keyboardType: TextInputType.phone,
-              textController: telefonoCtrl),
-              CustomDropDown(lista: listaGenero),
-          CustomButton(
-              text: 'Siguiente',
-              onPressed: () {
-                BlocProvider.of<SigninBloc>(context).add(CambiarPanel(2));
-              }),
-              SizedBox(height: 15),
-          CustomButton(
-              text: 'Regresar',
-              onPressed: () {
-                BlocProvider.of<SigninBloc>(context).add(CambiarPanel(0));
-              }),
-
-              LabelCancelar(subtitulo: 'Cancelar')
-        ],
-      ),
-    );
-  }
-}
-
-class _Form2 extends StatefulWidget {
-  @override
-  __Form2State createState() => __Form2State();
-}
-
-class __Form2State extends State<_Form2> {
-  final emailCtrl = TextEditingController();
-  final passCtrl = TextEditingController();
-  final pass2Ctrl = TextEditingController();
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 20),
-      padding: EdgeInsets.symmetric(horizontal: 50),
-      child: Column(
-        children: <Widget>[
-          CustomInput(
-            icon: Icons.mail_outline,
-            placeHolder: 'Correo',
-            keyboardType: TextInputType.emailAddress,
-            textController: emailCtrl,
-          ),
-          CustomInput(
-            icon: Icons.lock_outline,
-            placeHolder: 'Password',
-            isPassword: true,
-            textController: passCtrl,
-          ),
-          CustomInput(
-            icon: Icons.lock_outline,
-            placeHolder: 'Confirmar Password',
-            isPassword: true,
-            textController: pass2Ctrl,
-          ),
-          CustomButton(
-              text: 'Finalizar Registro',
-              onPressed: () {
-                BlocProvider.of<SigninBloc>(context).add(CambiarPanel(1));
-              }),
-              SizedBox(height: 15),
-          CustomButton(
-              text: 'Regresar',
-              onPressed: () {
-                BlocProvider.of<SigninBloc>(context).add(CambiarPanel(1));
-              }),
-              LabelCancelar(subtitulo: 'Cancelar')
-        ],
-      ),
-    );
-  }
-}
 
 
 
---
+++ lib/src/widgets/signIn_widget/forms.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_viaje_express_cliente/src/bloc/signIn_bloc/signin_bloc.dart';
+import 'package:flutter_viaje_express_cliente/src/widgets/custom_dropDown.dart';
+
+import '../custom_button.dart';
+import '../custom_input.dart';
+import '../labels.dart';
+
+// FORMULARIO CON LOS CAMPOS: CÉDULA, NOMBRES, APELLIDOS
+
+class Form0 extends StatefulWidget {
+  @override
+  FormState createState() => FormState();
+}
+
+class FormState extends State<Form0> {
+  final cedulaCtrl = TextEditingController();
+  final nombresCtrl = TextEditingController();
+  final apellidosCtrl = TextEditingController();
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: <Widget>[
+          CustomInput(
+              icon: Icons.perm_identity,
+              placeHolder: 'Cédula',
+              keyboardType: TextInputType.number,
+              textController: cedulaCtrl),
+          CustomInput(
+              icon: Icons.perm_identity,
+              placeHolder: 'Nombres',
+              keyboardType: TextInputType.name,
+              textController: nombresCtrl),
+          CustomInput(
+              icon: Icons.perm_identity,
+              placeHolder: 'Apellidos',
+              keyboardType: TextInputType.name,
+              textController: apellidosCtrl),
+
+          
+
+          CustomButton(
+              text: 'Siguiente',
+              onPressed: () {
+                BlocProvider.of<SigninBloc>(context).add(CambiarPanel(1));
+              }),
+          SizedBox(height: 35),
+          Labels(
+            ruta: 'login',
+            titulo: '¿Ya tienes cuenta?',
+            subtitulo: '¡Ingresa ahora!',
+          ),
+        ],
+      ),
+    );
+  }
+
+
+}
+
+
+
+
+// FORMULARIO CON LOS CAMPOS: TELÉFONO, FECHA DE NACIMIENTO, GÉNERO
+
+
+class Form1 extends StatefulWidget {
+  @override
+  Form1State createState() => Form1State();
+}
+
+class Form1State extends State<Form1> {
+  final telefonoCtrl = TextEditingController();
+   List<String> listaGenero = ['masculino', 'femenino', 'otro'];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: <Widget>[
+          CustomInput(
+              icon: Icons.perm_identity,
+              placeHolder: 'Teléfono',
+              keyboardType: TextInputType.phone,
+              textController: telefonoCtrl),
+
+              CustomDropDown(lista: listaGenero),
+
+          CustomButton(
+              text: 'Siguiente',
+              onPressed: () {
+                BlocProvider.of<SigninBloc>(context).add(CambiarPanel(2));
+              }),
+              SizedBox(height: 15),
+          CustomButton(
+              text: 'Regresar',
+              onPressed: () {
+                BlocProvider.of<SigninBloc>(context).add(CambiarPanel(0));
+              }),
+
+              LabelCancelar(subtitulo: 'Cancelar')
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+//FORMULARIO CON LOS CAMPOS: CORREO, PASSWORD, PASSWORD CONFIRMACIÓN
+
+class Form2 extends StatefulWidget {
+  @override
+  Form2State createState() => Form2State();
+}
+
+class Form2State extends State<Form2> {
+  final emailCtrl = TextEditingController();
+  final passCtrl = TextEditingController();
+  final pass2Ctrl = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: <Widget>[
+          CustomInput(
+            icon: Icons.mail_outline,
+            placeHolder: 'Correo',
+            keyboardType: TextInputType.emailAddress,
+            textController: emailCtrl,
+          ),
+          CustomInput(
+            icon: Icons.lock_outline,
+            placeHolder: 'Password',
+            isPassword: true,
+            textController: passCtrl,
+          ),
+          CustomInput(
+            icon: Icons.lock_outline,
+            placeHolder: 'Confirmar Password',
+            isPassword: true,
+            textController: pass2Ctrl,
+          ),
+          CustomButton(
+              text: 'Finalizar Registro',
+              onPressed: () {
+                BlocProvider.of<SigninBloc>(context).add(CambiarPanel(1));
+              }),
+              SizedBox(height: 15),
+          CustomButton(
+              text: 'Regresar',
+              onPressed: () {
+                BlocProvider.of<SigninBloc>(context).add(CambiarPanel(1));
+              }),
+              LabelCancelar(subtitulo: 'Cancelar')
+        ],
+      ),
+    );
+  }
+}

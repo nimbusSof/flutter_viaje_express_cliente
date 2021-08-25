96f661f7c01fe5ab6569880491c4759dca6454e8
Eduardo Olivo <keolivor@utn.edu.ec>
2021-08-24 21:41:58 -0500

    Se detecta cuándo el usuario activa el GPS

--- lib/src/pages/mapas/loading_page.dart
+++ lib/src/pages/mapas/loading_page.dart
@@ -2,28 +2,76 @@ import 'package:flutter/material.dart';
 import 'package:flutter_viaje_express_cliente/src/helpers/helpers.dart';
 import 'package:flutter_viaje_express_cliente/src/pages/mapas/acceso_gps_page.dart';
 import 'package:flutter_viaje_express_cliente/src/pages/mapas/mapa_page.dart';
+import 'package:geolocator/geolocator.dart' as Geolocator;
+import 'package:permission_handler/permission_handler.dart';
 
-class LoadingPage extends StatelessWidget {
+class LoadingPage extends StatefulWidget {
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance!.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance!.removeObserver(this);
+    super.dispose();
+  }
+
+   @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    if (state == AppLifecycleState.resumed) {
+      if (await Geolocator.Geolocator.isLocationServiceEnabled()) { // verifica que el servicio GPS este habilitado
+        Navigator.pushReplacement(context, navegarFadeIn(context, MapaPage()));
+      }
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder(
         future: this.checkGpsLocation(context),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
-          return Center(
-            child: CircularProgressIndicator(strokeWidth: 2),
-          );
+          if (snapshot.hasData) {
+            return Center(
+              child: Text(snapshot.data),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(strokeWidth: 2),
+            );
+          }
         },
       ),
     );
   }
 
   Future checkGpsLocation(BuildContext context) async {
-    // TODO: Persmisos GPS
-    // TODO: GPS está activo
+    // Persmisos GPS
+    final permisoGps = await Permission.location.isGranted;
+    // GPS está activo
+    final gpsActivo = await Geolocator.Geolocator.isLocationServiceEnabled();
 
-    await Future.delayed(Duration(milliseconds: 100));
-    print('loading page hola mundo');
+    if (permisoGps && gpsActivo) {
+      Navigator.pushReplacement(context, navegarFadeIn(context, MapaPage()));
+      return '';
+    } else if (!permisoGps) {
+      Navigator.pushReplacementNamed(
+          context, navegarFadeIn(context, AccesoGpsPage()));
+      return 'Es necesario el permiso del GPS';
+    } else {
+      return 'Porfavor Activo el GPS';
+    }
+
+    //await Future.delayed(Duration(milliseconds: 100));
+
     // Navigator.pushReplacementNamed(context, navegarFadeIn(context, AccesoGpsPage()));
     //Navigator.pushReplacement(context, navegarFadeIn(context, MapaPage()));
   }
